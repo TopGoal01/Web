25b5250bae193aa3226d8d96013ec7a729cf7dbd
신승민 <sky2197kr@gmail.com>
2021-08-02 16:04:56 +0900

    [Room] Video UI 구현

--- src/components/room/Video.tsx
+++ src/components/room/Video.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import YouTube from "react-youtube";
+import styled from 'styled-components';
 
 const Video: React.FC = () => {
+
+    const opts = {
+        width: "100%",
+        height: "700px",
+        playerVars: {
+          autoplay: undefined,
+        },
+      };
+    
+    const videoId = '51boqGNfH_8'
+
     return(
         <div>
-            비디오
+            <YouTube videoId={videoId} opts={opts} />
         </div>
     )
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '@/components/room/Header'
 import Video from '@/components/room/Video'
-import PlayList from '@/components/room/PlayList'
+import PlayList from '@/components/room/playList/PlayList'
 import Chat from '@/components/room/chat/Chat'
 import styled from "styled-components"
 import { Row, Col } from 'antd';
 
 const Room: React.FC = () => {
-    const headerHeight = 40
+    const headerHeight = 80
 
     const Room = styled.div`
         height: 100%;
@@ -19,19 +19,20 @@ const Room: React.FC = () => {
     `
 
     const CustomCol = styled(Col)`
-        padding: 20px;
+        padding-left: 20px;
         height: 100%;
     `
+
 
     return (
         <Room>
             <Header height={headerHeight}/>
             <CustomRow>
-                <Col span={16}>
+                <Col span={18}>
                     <Video/>
                     <PlayList/>
                 </Col>
-                <CustomCol span={8}>
+                <CustomCol span={6}>
                     <Chat/>
                 </CustomCol>
             </CustomRow>
