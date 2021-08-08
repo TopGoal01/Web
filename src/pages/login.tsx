2a1b341240d62da28660d6d40c0cc120a02376d1
Ojimin <ogm0205@gmail.com>
2021-08-08 18:49:44 +0900

    구글로그인 api 연동

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import * as React from 'react';
+import GoogleLogin from 'react-google-login';
 
-const Login: React.FC = () => {
+const Login = (props: any) => {
+    let googleClientId:string=process.env.REACT_APP_CLIENT_ID||"";
+    const [userObj, setUserObj]=React.useState({
+      email:"",
+      name:""
+    })
+    const onLoginSuccess=(res:any)=>{
+      setUserObj({...userObj,
+        email:res.profileObj.email,
+        name:res.profileObj.name
+      })
+      
+    }
     return (
-        <div>로그인 화면</div>
-    )
-}
-
+      <div>
+        <GoogleLogin
+          clientId = {googleClientId}
+          buttonText="Google"
+          onSuccess={result=>onLoginSuccess(result)}
+          onFailure={result => console.log(result)}
+        />
+  
+      </div>
+    );
+  };
+  
 export default Login;
