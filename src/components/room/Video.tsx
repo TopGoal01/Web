9345f4e85501e10747912c45b3481404ab19ff92
신승민 <sky2197kr@gmail.com>
2021-08-11 23:29:11 +0900

    [Room] Youtube iframe api 연결

--- src/components/room/Video.tsx
+++ src/components/room/Video.tsx
@@ -1,23 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import YouTube from "react-youtube";
 import styled from 'styled-components';
 
-const Video: React.FC = () => {
+declare global {
+    interface Window {
+      YT: any;
+    }
+  }
 
-    const opts = {
-        width: "100%",
-        height: "700px",
-        playerVars: {
-          autoplay: undefined,
-        },
-      };
+type VideoProps = {
+    playListHeight: number;
+}
+
+const Video: React.FC<VideoProps> = ({playListHeight}) => {
+
+    const [ player, setPlayer ] = useState()
+    const [ done, setDone ] = useState(false)
+
+    useEffect(() => {
+        onYouTubeIframeAPIReady()
+    }, [])
+
+    const onYouTubeIframeAPIReady = () => {
+        setPlayer(new window.YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+        }))
+    }
+
+    const onPlayerReady = (event: any) => {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+    const onPlayerStateChange = (event: any) => {
+        if (event.data == window.YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          setDone(true);
+        }
+      }
+
+    const stopVideo = () => {
+        console.log("stop")
+        // if(player) player?.stopVideo();
+    }
     
     const videoId = '51boqGNfH_8'
 
+    const playerStyle = {
+        width: '100%',
+        height: `calc(100% - ${playListHeight}px)`
+    }
+
     return(
-        <div>
-            <YouTube videoId={videoId} opts={opts} />
-        </div>
+        <div id="player" style={playerStyle}></div>
+        // <iframe width="100%" height="100%" src="https://www.youtube.com/embed/oqapAA7o-sc"/>
+        // <YouTube videoId={videoId} opts={opts} />
     )
 }
 
