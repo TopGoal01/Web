0f01a99485d84ddbe72482471381a2bfbea3544a
Ojimin <ogm0205@gmail.com>
2021-08-12 21:21:19 +0900

    구글로그인 api 연동 오류 해결

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,29 +2,30 @@ import * as React from 'react';
 import GoogleLogin from 'react-google-login';
 
 const Login = (props: any) => {
-    let googleClientId:string=process.env.REACT_APP_CLIENT_ID||"";
-    const [userObj, setUserObj]=React.useState({
-      email:"",
-      name:""
+  let googleClientId:string=process.env.CLIENT_ID||"";
+  const [userObj, setUserObj]=React.useState({
+    email:"",
+    name:""
+  })
+  const onLoginSuccess=(res:any)=>{
+    setUserObj({...userObj,
+      email:res.profileObj.email,
+      name:res.profileObj.name
     })
-    const onLoginSuccess=(res:any)=>{
-      setUserObj({...userObj,
-        email:res.profileObj.email,
-        name:res.profileObj.name
-      })
-      
-    }
-    return (
-      <div>
-        <GoogleLogin
-          clientId = {googleClientId}
-          buttonText="Google"
-          onSuccess={result=>onLoginSuccess(result)}
-          onFailure={result => console.log(result)}
-        />
+    
+  }
   
-      </div>
-    );
-  };
-  
+  return (
+    <div>
+      <GoogleLogin
+        clientId = {googleClientId}
+        buttonText="Google"
+        onSuccess={result=>onLoginSuccess(result)}
+        onFailure={result => console.log(result)}
+      />
+
+    </div>
+  );
+};
+
 export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const mode = process.env.NODE_ENV || 'development';
 
@@ -40,6 +41,7 @@ module.exports = {
         ],
     },
     plugins: [
+        new Dotenv(),
         new HtmlWebpackPlugin({
           template: './src/index.html',
           templateParameters: {
